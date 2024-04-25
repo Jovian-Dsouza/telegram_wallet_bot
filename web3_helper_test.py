6823e8229c34f1dc806234a9b2ae54e252164ecc
Jovian Dsouza <dsouzajovian123@gmai.com>
2024-04-25 22:07:44 +0530

    add testing scripts

--- web3_helper_test.py
+++ web3_helper_test.py
@@ -13,3 +13,9 @@ if __name__ == "__main__":
     account = wallet.get_account_from_private_key(private_key)
     balance = wallet.get_balance(account.address)
     print(f"Account Address: {account.address}\nWallet Balance: {balance} BNB")
+
+    recipient_address = "0x8db7e2f504c915A0aC40176581775c72186F888B"
+    wallet.send(account, recipient_address, 0.01)
+
+    balance = wallet.get_balance(account.address)
+    print(f"Account Address: {account.address}\nWallet Balance: {balance} BNB")
